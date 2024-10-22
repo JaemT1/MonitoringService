704468690cbc30627822226dd261adf876757b50
Juan Tunubala <juantunubala02@gmail.com>
2024-10-22 08:26:21 -0500

    V 0.2
    -Se modifican los formatos de respuesta del endpoint para añadir servicios.

--- src/main/java/org/example/monitoringservice/monitoringservice/Controllers/HealthController.java
+++ src/main/java/org/example/monitoringservice/monitoringservice/Controllers/HealthController.java
@@ -1,10 +1,14 @@
 package org.example.monitoringservice.monitoringservice.Controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.example.monitoringservice.monitoringservice.Entities.ApiError;
+import org.example.monitoringservice.monitoringservice.Entities.ApiSuccess;
 import org.example.monitoringservice.monitoringservice.Entities.MonitoredService;
 import org.example.monitoringservice.monitoringservice.Entities.MonitoredServiceDTO;
 import org.example.monitoringservice.monitoringservice.Services.HealthCheckService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,17 +27,35 @@ public class HealthController {
 
     // Registrar un nuevo microservicio para monitoreo
     @PostMapping("/register")
-    public ResponseEntity<MonitoredService> registerService(@Validated @RequestBody MonitoredServiceDTO serviceDTO) {
-        MonitoredService service = new MonitoredService();
-        service.setName(serviceDTO.getName());
-        service.setEndpoint(serviceDTO.getEndpoint());
-        service.setFrequency(serviceDTO.getFrequency());
-        service.setNotificationEmails(serviceDTO.getNotificationEmails());
-        service.setHealthy(true);  // Inicialmente asumimos que el servicio está healthy
-        service.setLastChecked(LocalDateTime.now());  // Establecer la hora actual como la última verificación inicial
+    public ResponseEntity<?> registerService(@Validated @RequestBody MonitoredServiceDTO serviceDTO, HttpServletRequest request) {
+        try {
+            MonitoredService service = new MonitoredService();
+            service.setName(serviceDTO.getName());
+            service.setEndpoint(serviceDTO.getEndpoint());
+            service.setFrequency(serviceDTO.getFrequency());
+            service.setNotificationEmails(serviceDTO.getNotificationEmails());
+            service.setHealthy(true);
+            service.setLastChecked(LocalDateTime.now());
 
-        MonitoredService savedService = healthCheckService.registerService(service);
-        return ResponseEntity.ok(savedService);
+            MonitoredService savedService = healthCheckService.registerService(service);
+
+            ApiSuccess apiSuccess = new ApiSuccess(
+                    HttpStatus.OK.value(),
+                    HttpStatus.OK.getReasonPhrase(),
+                    "Servicio añadido con exito",
+                    request.getRequestURI()
+            );
+
+            return ResponseEntity.ok(apiSuccess);
+        } catch (Exception e) {
+            ApiError apiError = new ApiError(
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                    HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                    "Error al añadir el servicio",
+                    request.getRequestURI()
+            );
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+        }
     }
 
     // Obtener el estado de salud de todos los microservicios registrados
